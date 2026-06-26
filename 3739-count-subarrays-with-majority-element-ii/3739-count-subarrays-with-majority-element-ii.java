f1de68dc5458108fd7a7c2a9264e37723b33151e
Saksham4486 <saksham4486@gmail.com>
2026-06-26 13:05:32 +0530

    Time: 2 ms (100%), Space: 88.2 MB (67.21%) - LeetHub

---
+++ 3739-count-subarrays-with-majority-element-ii/3739-count-subarrays-with-majority-element-ii.java
@@ -0,0 +1,26 @@
+class Solution {
+    public long countMajoritySubarrays(int[] nums, int target) {
+        int n=nums.length;
+        int pre[]=new int[2*n+1];
+        pre[n]=1;
+        int cnt=n;
+         long ans=0,presum=0;
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]==target)
+                {
+                    presum+=pre[cnt];
+                    cnt++;
+                    pre[cnt]++;
+                }
+                else
+                {
+                    cnt--;
+                    presum-=pre[cnt];
+                    pre[cnt]++;
+                }
+                ans+=presum;
+        }
+        return ans;
+    }
+}
